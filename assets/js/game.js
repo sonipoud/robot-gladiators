5c06872acf3a251749c5e6a2f18d5de8d27a4cee
Sonika <sonikapoudyal5@gmail.com>
2021-04-26 15:31:10 -0400

    added the functions of js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,24 @@
+var name= "Sonika Poudyal";
+console.log(name);
+ 
+var playerName = window.prompt("What is your robot's name?");
+//Note the lack of quotation marks around playerName window.alert(playerName);
+// What is this?
+var playerName = "Sam the Bot";
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a mesaage");
+//this willl do math and log 20
+console.log(10 + 10);
+//what is this?
+console.log("Our robot's name is" + playerName );
+ 
+//fight();
+ 
 window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+ 
+function fight() {
+   window.alert("The fight has begun!");
+}
+ 
+
